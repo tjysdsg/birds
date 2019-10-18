e7125d3aa024accc7b8c973a6f6b08e4029c329f
tjysdsg <tjy1018543509@163.com>
2019-10-18 16:41:57 +0800

    update

--- generate-report.py
+++ generate-report.py
@@ -1,8 +1,11 @@
 import csv
 import os
 import sys
+from collections import Counter
 
 if __name__ == "__main__":
+    if len(sys.argv) == 0:
+        print("Need to specify the location of the dataset")
     img_dir = sys.argv[1]
     bird_eng_names = []
     
@@ -20,9 +23,15 @@ if __name__ == "__main__":
         if os.path.isdir(bird_dir):
             bird_img_count[i] = len(os.listdir(bird_dir))
 
+    counter = Counter(bird_img_count)
     # write report
     with open('report.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for i in range(n_birds):
             writer.writerow([bird_eng_names[i], bird_img_count[i]])
 
+    with open('n_images_count.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        n_counters = len(counter.keys())
+        for i in range(n_counters):
+            writer.writerow([list(counter.keys())[i], list(counter.values())[i]])
